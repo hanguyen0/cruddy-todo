bcac9421ba00bb838a2411cd27988327c7bc3c3f
uxsasukexnaruto <nguyha16@gmail.com>
2020-04-14 12:37:29 -0700

    pass all tests

--- datastore/index.js
+++ datastore/index.js
@@ -33,10 +33,10 @@ exports.readAll = (callback) => {
   // });
   fs.readdir(exports.dataDir, (err, files) => {
     if (err) {
-      console.log("err inside readAll");
+      console.log('err inside readAll');
     } else {
       let data = _.map(files, (text, id) => {
-        let num = text.split(".");
+        let num = text.split('.');
         text = num[0];
         id = text;
         return { id, text };
@@ -52,7 +52,7 @@ exports.readOne = (id, callback) => {
   // var text = items[id];
   fs.readFile(`${exports.dataDir}/${id}.txt`, (err, fileData) => {
     if (err) {
-      console.log("err from readOne");
+      console.log('err from readOne');
       callback(new Error(`No item with id: ${id}`));
     } else {
       // fileData = fileData.toString();
@@ -61,32 +61,53 @@ exports.readOne = (id, callback) => {
       // } else {
       //   callback(null, { id, fileData });
       // }
-      let data = {id: id, text: fileData.toString()};
+      let data = { id: id, text: fileData.toString() };
       callback(null, data);
     }
-  })
+  });
 
 };
 
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
+  // var item = items[id];
+  // let updatedData = text
+  if (fs.existsSync(`${exports.dataDir}/${id}.txt`)) {
+    fs.writeFile(`${exports.dataDir}/${id}.txt`, text, (err) => {
+      if (err) {
+        callback(new Error(`No item with id: ${id}`));
+      } else {
+        callback(null, { id, text });
+      }
+    });
   } else {
-    items[id] = text;
-    callback(null, { id, text });
+    callback(new Error(`No item with id: ${id}`));
   }
+
+  // if (!item) {
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   items[id] = text;
+  //   callback(null, { id, text });
+  // }
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  // var item = items[id];
+  // delete items[id];
+  fs.unlink(`${exports.dataDir}/${id}.txt`, (err) => {
+    if (err) {
+      console.log('err inside delete');
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      callback();
+    }
+  })
+  // if (!item) {
+  //   // report an error if item not found
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   callback();
+  // }
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
